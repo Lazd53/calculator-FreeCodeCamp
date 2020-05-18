1af77ca0709803197974331fbbec01265cfe692d
Alex Love <lazd53@gmail.com>
2020-05-18 16:12:55 -0700

    CSS for components created, cleanup of HTML

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import CalculatorBody from './CalculatorBody';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CalculatorBody />
   </React.StrictMode>,
   document.getElementById('root')
 );
